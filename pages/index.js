64a6686f0b92f739cff469265367a8e359bd5779
Edwin <65671061+edw-in@users.noreply.github.com>
2022-08-07 12:52:20 +0100

    Renamed title

--- pages/index.js
+++ pages/index.js
@@ -72,7 +72,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Compiler</title>
+        <title>ExecutorX</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
